6d7085d6d1e5561c23a4dafe76a05972b574aa06
Kenneth <liangk1994@gmail.com>
2017-10-06 11:26:43 -0700

    First commit

---
+++ Library/Collab/Original/Assets/Warrior Pack Bundle 1 FREE/Code/WarriorAnimationDemoFREE.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarriorAnimationDemoFREE : MonoBehaviour 
+{
+	public Animator animator;
+
+	float rotationSpeed = 30;
+	public bool stunned = false;
+	public Behaviour movement;
+	public Collider hitbox;
+
+	Vector3 inputVec;
+	Vector3 targetDirection;
+	
+	//Warrior types
+	public enum Warrior{Karate, Ninja, Brute, Sorceress, Knight, Mage, Archer, TwoHanded, Swordsman, Spearman, Hammer, Crossbow};
+    public string horizontal_input;
+    public string vertical_input;
+	public string AttackInput;
+    public Warrior warrior;
+	
+	void Update()
+	{
+		//Get input from controls
+		float z = Input.GetAxisRaw(horizontal_input);
+		float x = -(Input.GetAxisRaw(vertical_input));
+		inputVec = new Vector3(x, 0.0f, z);
+
+		//Apply inputs to animator
+		animator.SetFloat("Input X", z);
+		animator.SetFloat("Input Z", -(x));
+
+		if (x != 0 || z != 0 )  //if there is some input
+		{
+			//set that character is moving
+			animator.SetBool("Moving", true);
+			animator.SetBool("Running", true);
+		}
+		else
+		{
+			//character is not moving
+			animator.SetBool("Moving", false);
+			animator.SetBool("Running", false);
+		}
+
+		if (Input.GetButtonDown(AttackInput))
+		{
+			animator.SetTrigger("Attack1Trigger");
+			if (warrior == Warrior.Brute)
+				StartCoroutine (COStunPause(1.2f));
+			else if (warrior == Warrior.Sorceress)
+				StartCoroutine (COStunPause(1.2f));
+			else
+				StartCoroutine (COStunPause(.6f));
+			
+		}
+
+		UpdateMovement();  //update character position and facing
+		
+		if(stunned == true){
+			animation.enabled = false
+			StartCoroutine (COStunPause(3.0f));
+			stunned = false;
+			animation.enabled = true;
+		}
+	}
+	void LaunchAttack(Collider other){
+		Collider[] cols = Physics.OverlapBox(col.bounds.center,col.bounds.extends,col.transform.rotation,LayerMask.GetMask("Hitbox"));
+		foreach(Collider c in cols){
+			if(other.gameObject.tag == "player"){
+				other.gameObject.isStunned();
+		}
+		}
+		
+		
+	}
+
+	void isStunned(){
+		stunned = true;
+	}
+
+	public IEnumerator COStunPause(float pauseTime)
+	{
+		yield return new WaitForSeconds(pauseTime);
+	}
+
+	//converts control input vectors into camera facing vectors
+	void TargetDirectionUpdate()
+	{  
+		
+
+		// Target direction update
+		targetDirection = -(inputVec) ;
+	}
+
+	//face character along input direction
+	void RotateTowardMovementDirection()  
+	{
+		if (inputVec != Vector3.zero)
+		{
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDirection), Time.deltaTime * rotationSpeed);
+		}
+	}
+
+	void UpdateMovement()
+	{
+		//get movement input from controls
+		Vector3 motion = inputVec;
+
+		//reduce input for diagonal movement
+		motion *= (Mathf.Abs(inputVec.x) == 1 && Mathf.Abs(inputVec.z) == 1) ? 0.7f:1;
+
+		RotateTowardMovementDirection();  
+		TargetDirectionUpdate();  
+	}
+
+	void OnGUI () 
+	{
+		if (GUI.Button (new Rect (25, 85, 100, 30), "Attack1")) 
+		{
+			animator.SetTrigger("AttackTrigger");
+
+			if (warrior == Warrior.Brute || warrior == Warrior.Sorceress)  //if character is Brute or Sorceress
+				StartCoroutine (COStunPause(1.2f));
+			else
+				StartCoroutine (COStunPause(.6f));
+		}
+	}
+}
